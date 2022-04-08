f6898e271cc4016aecf38abfad615da876691f37
MFG38 <santtu.pesonen@mail.com>
2022-04-08 16:28:00 +0300

    Small balance tweaks

--- _build.bat
+++ _build.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERNUM=v20-pre-r369
+set VERNUM=v20-pre-r370
 
 echo Building...
 _tools\7za a -tzip sgd_%VERNUM%.pk3 *.* *  -x!.gitignore -x!_build.bat -xr!.git -xr!_build -xr!_tools
