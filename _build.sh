4fd2d522a9c77f0d2d5c3ede57ce58375768cb55
MFG38 <santtu.pesonen@mail.com>
2022-11-15 18:33:25 +0200

    Slight weapon pickup sound volume reduction

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v20-pre-r392"
+VERNUM="v20-pre-r394"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
