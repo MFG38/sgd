60c12d36bf49d4a93626c18cce3a53730bd68166
MFG38 <santtu.pesonen@mail.com>
2022-11-27 15:34:56 +0200

    Bringing back the BD anti-compat layer

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v20-pre-r394"
+VERNUM="v20-pre-r395"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
