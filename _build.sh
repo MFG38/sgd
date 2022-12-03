22e805b06478bbb5671b6d8db2127006b57d65a4
MFG38 <santtu.pesonen@mail.com>
2022-12-03 18:31:43 +0200

    fuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuck

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v20-pre-r410"
+VERNUM="v20-pre-r411"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
