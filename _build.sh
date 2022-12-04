319f0d05eafd1b1caa5196c262177e7dad8f7f02
MFG38 <santtu.pesonen@mail.com>
2022-12-04 14:45:36 +0200

    Some fancy alpha modulation shit

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v20-pre-r415"
+VERNUM="v20-pre-r416"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
