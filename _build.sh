4b802105d0fb25e41d1891809bf8e29e85089140
MFG38 <santtu.pesonen@mail.com>
2022-12-10 22:33:12 +0200

    Add option menu tooltips

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v21-pre-r426"
+VERNUM="v21-pre-r427"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
