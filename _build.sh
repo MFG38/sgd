84cba6c7cde9983844365a4fa0340df5df8c1fbb
MFG38 <santtu.pesonen@mail.com>
2023-02-21 18:13:06 +0200

    Finisher bar shouldn't obstruct stats in automap view

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v211-pre-r436"
+VERNUM="v211-pre-r437"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
