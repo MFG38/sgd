94b6ca697a1fd7e2131d29e3252176667f340386
MFG38 <santtu.pesonen@mail.com>
2024-01-21 21:51:54 +0200

    Force LF line endings in _build.sh

--- _build.sh
+++ _build.sh
@@ -2,5 +2,5 @@
 VERNUM="v211"
 
 echo Building...
-7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
+7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
 mv sgd_${VERNUM}.pk3 _build
