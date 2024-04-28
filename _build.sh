e749f0caf7bf0cecf26432ecd31681d91fcba269
MFG38 <santtu.pesonen@mail.com>
2024-04-28 15:08:39 +0300

    I'm getting nowhere with fixing the feat system bug.

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-VERNUM="v22-pre-r462"
+VERNUM="v22-pre-r463"
 
 echo Building...
 7za a -tzip sgd_${VERNUM}.pk3 * -x!.gitattributes -x!.gitignore -x!_build.bat -x!_build.sh -xr!.git -xr!_build -xr!_tools
